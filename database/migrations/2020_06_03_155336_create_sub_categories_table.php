cd8376aa03c8073f63b63f2c2c5d5743f1862691
tadasbudrys <tadas.budrys@outlook.com>
2020-06-03 19:02:03 +0300

    Created DB

---
+++ database/migrations/2020_06_03_155336_create_sub_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( 'subcategory', function ( Blueprint $table ) {
+            $table->increments( 'id' );
+            $table->string( 'name', 200 );
+
+
+        } );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_categories');
+    }
+}
---
+++ database/migrations/2020_06_03_155353_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+
+            $table->increments( 'id' );
+            $table->integer( 'category_id' )->unsigned();
+            $table->integer( 'subcategory_id' )->unsigned();
+            $table->string( 'title' );
+        });
+
+        Schema::table('product', function (Blueprint $table) {
+            $table->foreign( 'category_id' )
+                ->references( 'id' )->on( 'category' )
+                ->onDelete( 'cascade' );
+
+            $table->foreign( 'subcategory_id' )
+                ->references( 'id' )->on( 'subcategory' )
+                ->onDelete( 'cascade' );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
